9b72a45c531df091802a1a96afa5095a56655274
jguttierrez368@gmail.com <jguttierrez368@gmail.com>
2018-02-13 22:31:36 -0500

    Jeffry 22:31

--- app/src/main/java/pe/edu/sise/appportales/LoginActivity.java
+++ app/src/main/java/pe/edu/sise/appportales/LoginActivity.java
@@ -2,14 +2,16 @@ package pe.edu.sise.appportales;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
-
-
+    protected Button btnIniciar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnIniciar= (Button) findViewById(R.id.btnIniciar);
     }
 }
