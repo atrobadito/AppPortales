5d29c2073f248d27b79759518fbcc67067b3d3ab
jguttierrez368@gmail.com <jguttierrez368@gmail.com>
2018-02-13 22:00:25 -0500

    Subiendo Login

--- app/src/main/java/pe/edu/sise/appportales/MainActivity.java
+++ app/src/main/java/pe/edu/sise/appportales/MainActivity.java
@@ -1,13 +1,34 @@
 package pe.edu.sise.appportales;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //primer commit de este proyecto
+    protected Button btningresar,btnregistrese;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btningresar= (Button) findViewById(R.id.btningresar);
+        btnregistrese=(Button)findViewById(R.id.btnregistrar);
+    }
+
+    public void irLogin(View view){
+        Intent intent=new Intent(
+                getApplicationContext(),
+                LoginActivity.class);
+        startActivity(intent);
+    }
+    public void irRegistrarse(View view){
+        Intent intent=new Intent(
+                getApplicationContext(),
+                RegistreseActivity.class);
+        startActivity(intent);
     }
 }
